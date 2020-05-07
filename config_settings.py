c420e29996abd9f5f4be7c969c953118a7e2088c
DTradd12 <derektradd@gmail.com>
2020-05-06 21:20:38 -0400

    config settings

---
+++ config_settings.py
@@ -0,0 +1,12 @@
+AUTH0_DOMAIN = 'dtradd.auth0.com'
+JWT = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjY3bE5XenRSWEpIaF8xWkFXOGdLcyJ9' \
+     '.eyJpc3MiOiJodHRwczovL2R0cmFkZC5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU5NDkyMTAyNGU0YmMwY' \
+     'mUzNzM1ZjI2IiwiYXVkIjoiQ2FzdGluZ0FnZW5jeSIsImlhdCI6MTU4ODgxMjA0NiwiZXhwIjoxNTg4ODk4NDQ2L' \
+     'CJhenAiOiJwd2RnZGFxcDg5SXdkbnZnM2N3alpvbE14M0VIM3NhOSIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOl' \
+     'siZGVsZXRlOmFjdG9yIiwiZGVsZXRlOm1vdmllIiwiZWRpdDphY3RvciIsImVkaXQ6bW92aWUiLCJnZXQ6YWN0b3' \
+     'JzIiwiZ2V0Om1vdmllcyIsInBvc3Q6YWN0b3IiLCJwb3N0Om1vdmllIl19.wXIyCQIOe78sH58KKeMrifVY5-6pO' \
+     'Wa5TxrX6c9BPNmkeBOjDq7L5dKEYk-OLDt8ZXXmW0JkIeMCtfQ5HCgmHR-e6qnezrclcki5WawhKBzpF-4zw3jdG' \
+     '9Z9uY7wLFqi6c41lD6I_YEmzI-psK5QeaH7s1uifEOFoyowO2ADbvyCyRRN9LxerAoJNU261VC4eq917CgujWOj' \
+     'LM2ETw0_DVlx17KjRq-UgqUUqFIIe8fB3k7_pYBLLDbudcDEhkA9Cl7OmC5LJNX6rHMQLCiwHzwWFdsuQkwmPBc' \
+     'u1xQkqK5qh0fcC6sqNxIIZZp2OYJRwj1daiA-9mtcGEBZ99m1kQ'
+AUTH0_CLIENT_ID = 'pwdgdaqp89Iwdnvg3cwjZolMx3EH3sa9'
