35a19b636333a4007684b862c96d8302c0e7dc75
DTradd12 <derektradd@gmail.com>
2020-05-06 21:12:53 -0400

    Adding comments

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def create_app(test_config=None):
     @app.route("/actors", methods=['GET'])
     @requires_auth(permission='get:actors')
     def get_actors(*args, **kwargs):
+        """Retrieves all actors"""
         actors = Actor.query.order_by(Actor.id)
         actor_list = [actor.formatted() for actor in actors]
 
@@ -39,6 +40,7 @@ def create_app(test_config=None):
     @app.route("/movies", methods=['GET'])
     @requires_auth(permission='get:movies')
     def get_movies(jwt):
+        """Retrieves all movies"""
         movies = Movie.query.order_by(Movie.id)
         movie_list = [movie.formatted() for movie in movies]
 
@@ -52,6 +54,7 @@ def create_app(test_config=None):
     @app.route("/actors/<int:actor_id>", methods=['GET'])
     @requires_auth(permission='get:actors')
     def get_actor(jwt, actor_id):
+        """Gets an individual actor."""
         actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
 
         if actor is None:
@@ -66,6 +69,7 @@ def create_app(test_config=None):
     @app.route("/movies/<int:movie_id>", methods=['GET'])
     @requires_auth(permission='get:movies')
     def get_movie(jwt, movie_id):
+        """Gets an individual movie."""
         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
 
         if movie is None:
@@ -80,6 +84,7 @@ def create_app(test_config=None):
     @app.route("/actors/<int:actor_id>", methods=['DELETE'])
     @requires_auth(permission='delete:actor')
     def delete_actor(jwt, actor_id):
+        """Deletes an actor."""
         actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
 
         if actor is None:
@@ -95,6 +100,7 @@ def create_app(test_config=None):
     @app.route("/movies/<int:movie_id>", methods=['DELETE'])
     @requires_auth(permission='delete:movie')
     def delete_movie(jwt, movie_id):
+        """Deletes a movie."""
         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
 
         if movie is None:
@@ -110,6 +116,7 @@ def create_app(test_config=None):
     @app.route("/movies/create", methods=['POST'])
     @requires_auth(permission='post:movie')
     def add_movie(jwt):
+        """Creates a movie."""
         body = request.get_json()
 
         try:
@@ -127,6 +134,7 @@ def create_app(test_config=None):
     @app.route("/actors/create", methods=['POST'])
     @requires_auth(permission='post:actor')
     def add_actor(jwt):
+        """Creates an actor."""
         body = request.get_json()
 
         try:
@@ -144,7 +152,7 @@ def create_app(test_config=None):
     @app.route("/movies/<int:movie_id>", methods=['PATCH'])
     @requires_auth(permission='edit:movie')
     def edit_movie(payload, movie_id):
-
+        """Edits a movies data."""
         movie = Movie.query.get(movie_id)
 
         if not movie:
@@ -173,7 +181,7 @@ def create_app(test_config=None):
     @app.route("/actors/<int:actor_id>", methods=['PATCH'])
     @requires_auth(permission='edit:actor')
     def edit_actor(payload, actor_id):
-
+        """Edits an actors data."""
         actor = Actor.query.get(actor_id)
 
         if not actor:
